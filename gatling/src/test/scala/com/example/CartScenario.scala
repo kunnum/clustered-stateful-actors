b750d4bb4ec8276f458ded2ac86f0fabf5de5d5d
Meeraj Kunnumpurath <meeraj@servicesymphony.com>
2020-04-25 16:53:12 +0400

    Working on Gatling load test

--- gatling/src/test/scala/com/example/CartScenario.scala
+++ gatling/src/test/scala/com/example/CartScenario.scala
@@ -8,89 +8,34 @@ import scala.util.Random
 import io.gatling.core.Predef._
 import io.gatling.http.Predef._
 
-class ArtifactStateScenario
-  extends Simulation {
+class CartScenario extends Simulation {
 
   private val config =  ConfigFactory.load()
 
   val baseUrl = config.getString("loadtest.baseUrl")
 
-  val namesFeeder = csv("lastnames.csv").random
+  val cartIds = Iterator.continually(
+    Map("cartId" -> Random.nextInt(10))
+  )
 
-  val artifactIds = Iterator.continually(
-    // Random number will be accessible in session under variable "artifactId"
-    Map("artifactId" -> Random.nextInt(500))
+  val ids = Iterator.continually(
+    Map("id" -> Random.nextInt(100))
   )
 
   val httpConf = http
-    .baseUrl(s"${baseUrl}/artifactState")
-    .acceptHeader("application/json")
+    .baseUrl(s"${baseUrl}/cart")
 
-  val artifactAndUser = StringBody("""{ "artifactId": ${artifactId}, "userId": "${name}" }""")
-
-  // a scenario that simply runs through all the various state changes
-  val scn = scenario("ArtifactStateScenario")
-    .feed(namesFeeder)
-    .feed(artifactIds)
+  val scn = scenario("CartScenario")
+    .feed(cartIds)
+    .feed(ids)
     .exec(
-      http("set_artifact_read")
-      .post("/setArtifactReadByUser")
-      .body(artifactAndUser).asJson
+      http("post_item")
+      .get("/${cartId}?id=${id}&quantity=10")
       .check(status.is(200))
     )
 
-    .exec(
-      http("is_artifact_read")
-        .post("/isArtifactReadByUser")
-        .body(artifactAndUser).asJson
-        .check(status.is(200))
-    )
-
-    .exec(
-      http("set_artifact_in_feed")
-        .post("/setArtifactAddedToUserFeed")
-        .body(artifactAndUser).asJson
-        .check(status.is(200))
-    )
-
-    .exec(
-      http("is_artifact_in_user_feed")
-        .post("/isArtifactInUserFeed")
-        .body(artifactAndUser).asJson
-        .check(status.is(200))
-    )
-
-    .exec(
-      http("set_artifact_removed_from_feed")
-        .post("/setArtifactRemovedFromUserFeed")
-        .body(artifactAndUser).asJson
-        .check(status.is(200))
-    )
-
-    .exec(
-      http("get_all_states")
-        .post("/setArtifactReadByUser")
-        .body(artifactAndUser).asJson
-        .check(status.is(200))
-    )
-
   setUp(
-//    scn.inject(atOnceUsers(1))
-//    scn.inject(rampUsers(100) during (3 minutes))
-    scn.inject(rampUsers(1000) during (5 minutes))
-    // simulation set up -> https://gatling.io/docs/current/general/simulation_setup/
-/*
-    scn.inject(
-      nothingFor(4 seconds), // 1
-      atOnceUsers(10), // 2
-      rampUsers(10) during (5 seconds), // 3
-      constantUsersPerSec(20) during (15 seconds), // 4
-      constantUsersPerSec(20) during (15 seconds) randomized, // 5
-      rampUsersPerSec(100) to 20 during (10 minutes), // 6
-      rampUsersPerSec(100) to 20 during (10 minutes) randomized, // 7
-      heavisideUsers(1000) during (20 seconds) // 8
-    )
-*/
+    scn.inject(rampUsers(100) during (1 minutes))
     .protocols(httpConf)
   )
 }
